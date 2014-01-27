361a383f521b8ebeb0448cb749ba6ae36fba9163
Michal Szyndel <michal.szyndel@me.com>
2014-01-27 11:34:23 +0100

    fix routes for rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 PayU::Engine.routes.draw do
-  match '/payment-positive/:session_id/:error_code' => 'payments#positive'
-  match '/payment-negative/:session_id/:error_code' => 'payments#negative'
-  match '/payment-report' => 'payments#report'
+  match '/payment-positive/:session_id/:error_code' => 'payments#positive', via: :all
+  match '/payment-negative/:session_id/:error_code' => 'payments#negative', via: :all
+  match '/payment-report' => 'payments#report', via: :all
 end
